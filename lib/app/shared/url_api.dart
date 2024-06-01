80eb132845c29be53b3e0d044224cb65925a2a66
Alf4r <malfarizky23@gmail.com>
2024-06-01 11:14:10 +0700

    update

--- lib/app/shared/url_api.dart
+++ lib/app/shared/url_api.dart
@@ -2,8 +2,8 @@ class UrlApi {
   static String baseUrl = 'https://dksystem.id/api/';
   static String urlStorage = 'https://dksystem.id/storage/';
 
-  static String baseUrl = 'http://192.168.196.181:8000/api/';
-  static String urlStorage = 'http://192.168.196.181:8000/storage/';
+  // static String baseUrl = 'http://192.168.196.181:8000/api/';
+  // static String urlStorage = 'http://192.168.196.181:8000/storage/';
   static String register = '${baseUrl}register';
   static String login = '${baseUrl}login';
   static String logout = '${baseUrl}logout';
